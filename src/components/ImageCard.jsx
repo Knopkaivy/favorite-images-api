b2811394d66b18bbcdee2c3b2bd4f8d4da6e02cb
Knopka <knopkaivy@gmail.com>
2023-08-06 15:11:39 -0500

    update styles for ImageCard, add hover effects

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { FaPlus } from 'react-icons/fa';
 import '../styles/ImageCard.css';
 
 const ImageCard = ({ image }) => {
   return (
     <Card className="ImageCard">
+      <div className="ImageCard__overlay"></div>
+      <div className="ImageCard__btnContainer">
+        <button className="ImageCard__btn">
+          <FaPlus color="white" />
+        </button>
+      </div>
       <Card.Img
         variant="top"
         src={image.urls.small}
         className="ImageCard__img"
       />
-      <Card.Body>
+      <Card.Body className="ImageCard__body">
         <Card.Text>
           <a
             href={image.links.html}
