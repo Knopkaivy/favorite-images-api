08bfd40fdcca052f3139e43f3c5adf14d3ae9c02
Knopka <knopkaivy@gmail.com>
2023-08-11 17:29:17 -0500

    add Link to ImageCard

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import { FaCheck, FaPlus } from 'react-icons/fa';
 import { IoClose } from 'react-icons/io5';
@@ -19,32 +19,53 @@ const ImageCard = ({ image }) => {
     }
   }, [done]);
   const handleAddFavorite = (event) => {
-    event.stopPropagation();
+    event.preventDefault();
+    console.log(image);
     dispatchFavorite({ type: 'added', image });
     setDone(true);
   };
   const handleRemoveFavorite = (event) => {
-    event.stopPropagation();
+    event.preventDefault();
     window.confirm('Remove image from Favorite?');
     dispatchFavorite({ type: 'removed', image });
     setDone(true);
   };
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      if (event.target.id === 'btnAdd') {
+        handleAddFavorite(event);
+      } else {
+        handleRemoveFavorite(event);
+      }
+    }
+  };
   return (
-    <Card className="ImageCard" tabIndex={0} aria-label="image card">
+    <Link
+      to={image.links.html}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="ImageCard card mb-4"
+      aria-label="Image card. Open home location of image on Unsplash.com in a new tab"
+    >
       <div className="ImageCard__overlay" aria-hidden="true"></div>
       <div className="ImageCard__btnContainer">
         {location.pathname.includes('favorite') ? (
           <button
+            id="btnRemove"
             className="ImageCard__btn"
             onClick={(e) => handleRemoveFavorite(e)}
+            onKeyDown={(e) => handleKeyDown(e)}
             aria-label="remove this image from favorite"
           >
             <IoClose color="white" aria-hidden="true" />
           </button>
         ) : (
           <button
+            id="btnAdd"
             className="ImageCard__btn"
             onClick={(e) => handleAddFavorite(e)}
+            onKeyDown={(e) => handleKeyDown(e)}
             aria-label="add this image to favorite"
           >
             {done ? (
@@ -55,26 +76,18 @@ const ImageCard = ({ image }) => {
           </button>
         )}
       </div>
-      <a
-        href={image.links.html}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="stretched-link"
-        aria-label="open home location of image on Unsplash.com in a new tab"
-      >
-        <Card.Img
-          variant="top"
-          src={image.urls.small}
-          className="ImageCard__img"
-          alt={image.alt_description}
-        />
-        <Card.Body className="ImageCard__body" aria-hidden="true">
-          <Card.Text className="ImageCard__description">
-            {image.description || image.alt_description}
-          </Card.Text>
-        </Card.Body>
-      </a>
-    </Card>
+      <Card.Img
+        variant="top"
+        src={image.urls.small}
+        className="ImageCard__img"
+        alt={image.alt_description}
+      />
+      <Card.Body className="ImageCard__body" aria-hidden="true">
+        <Card.Text className="ImageCard__description">
+          {image.description || image.alt_description}
+        </Card.Text>
+      </Card.Body>
+    </Link>
   );
 };
 
